11f9b2af7f7a64f12e925c28acb4e3b3cb4326b5
tarkptel <tspatel462@gmail.com>
2025-12-06 11:13:24 +0530

    21

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello I'm a Tark Patel from Flask CI/CD App.!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+print('Hello its ts')
