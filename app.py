4d7cadb2dc69bdf9982bb5c981a26c6e2aeb78c6
tarkptel <tspatel462@gmail.com>
2025-12-13 10:13:28 +0530

    51

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import flask
+# import flask
 
-print(flask.__version__)
+# print(flask.__version__)
+
+
+def xy(a,b):
+    if a == 0:
+        sum = a+b
+    return sum
+
+s = xy(0,23)
+
+print(s)
